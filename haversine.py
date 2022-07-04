542837cf494c0882452acc690b938d90ee41ce42
SNEYDER RAMIREZ <87345230+sramirez88@users.noreply.github.com>
2022-07-04 11:54:19 -0500

    Add files via upload

---
+++ haversine.py
@@ -0,0 +1,10 @@
+import math
+
+def haversine(lat1, lon1, lat2, lon2):
+    rad=math.pi/180
+    dlat=lat2-lat1
+    dlon=lon2-lon1
+    R=6372.795477598
+    a=(math.sin(rad*dlat/2))**2 + math.cos(rad*lat1)*math.cos(rad*lat2)*(math.sin(rad*dlon/2))**2
+    distancia=2*R*math.asin(math.sqrt(a))
+    return distancia
